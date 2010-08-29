189481fcdb4202a1c9a9ce718114d1ca335b3d7f
rcrowell <crowell@alum.mit.edu>
2010-08-29 13:20:13 -0400

    Updated the code to remove dependencies on the external lastfm library.  Just dumping the plain lastfm xml response into the body of the page.

--- double_my_playlist/double_my_playlist/controllers/doubler.py
+++ double_my_playlist/double_my_playlist/controllers/doubler.py
@@ -1,5 +1,6 @@
 import lastfm
 import logging
+import urllib
 
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
@@ -8,10 +9,17 @@ from double_my_playlist.lib.base import BaseController, render
 
 log = logging.getLogger(__name__)
 
+LASTFM_API_PREFIX = 'http://ws.audioscrobbler.com/2.0/?'
+LASTFM_API_KEY = '578ff28eb42a625b4beb7cd1ec8c1dae'
+
 class DoublerController(BaseController):
 
     def index(self, artist='The Temper Trap', track='Fader'):
-        api = lastfm.Api('578ff28eb42a625b4beb7cd1ec8c1dae')
-        track = api.get_track(track, artist=artist)
-        c.tracks = track.similar
+        params = {'method': 'track.getSimilar',
+                  'limit': 10,
+                  'api_key': LASTFM_API_KEY,
+                  'artist': artist,
+                  'track': track}
+        c.data = urllib.urlopen(LASTFM_API_PREFIX + urllib.urlencode(params)).read()
+        c.tracks = []
         return render('doubler/index.html')
