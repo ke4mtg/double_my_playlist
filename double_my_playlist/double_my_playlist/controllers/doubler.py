c4ac33082507f4d283fbef17cca2d28203436104
rcrowell <crowell@alum.mit.edu>
2010-08-31 03:25:03 -0400

    Made a version of the UI that takes a list.

--- double_my_playlist/double_my_playlist/controllers/doubler.py
+++ double_my_playlist/double_my_playlist/controllers/doubler.py
@@ -23,22 +23,32 @@ class Track(object):
         self.name = name
         self.artist = artist
 
-
+    
 class DoublerController(BaseController):
 
     def index(self, artist='The Temper Trap', track='Fader'):
-        params = {'method': 'track.getSimilar',
-                  'limit': 10,
-                  'format': 'json',
-                  'api_key': LASTFM_API_KEY,
-                  'artist': artist,
-                  'track': track}
+        def find_similar(artist, track):
+            params = {'method': 'track.getSimilar',
+                      'limit': 25,
+                      'format': 'json',
+                      'api_key': LASTFM_API_KEY,
+                      'artist': artist,
+                      'track': track}
+            data = json.loads(urllib.urlopen(LASTFM_API_PREFIX + urllib.urlencode(params)).read())
+    
+            tracks = []
+            for d in data['similartracks']['track']:
+                artist = Artist(d['artist']['name'])
+                track = Track(d['name'], artist)
+                tracks.append(track)
+            return tracks
+        
+        c.playlist = request.params.get('playlist', 'The Temper Trap - Fader\nPearl Jam - Once')
 
+        known_tracks = [x.split(' - ') for x in c.playlist.split('\n')]
         c.tracks = []
-        data = json.loads(urllib.urlopen(LASTFM_API_PREFIX + urllib.urlencode(params)).read())        
-        for d in data['similartracks']['track']:
-            artist = Artist(d['artist']['name'])
-            track = Track(d['name'], artist)
-            c.tracks.append(track)
+        for artist, track in known_tracks:
+            for similar_track in find_similar(artist, track):
+                c.tracks.append(similar_track)
             
         return render('doubler/index.html')
