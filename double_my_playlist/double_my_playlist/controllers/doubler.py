cdd9ec0b36b9c8b787280a74f752087ed381aa40
rcrowell <crowell@alum.mit.edu>
2010-08-31 01:53:08 -0400

    Parsing the json response from lastfm

--- double_my_playlist/double_my_playlist/controllers/doubler.py
+++ double_my_playlist/double_my_playlist/controllers/doubler.py
@@ -1,3 +1,4 @@
+import json
 import lastfm
 import logging
 import urllib
@@ -12,14 +13,32 @@ log = logging.getLogger(__name__)
 LASTFM_API_PREFIX = 'http://ws.audioscrobbler.com/2.0/?'
 LASTFM_API_KEY = '578ff28eb42a625b4beb7cd1ec8c1dae'
 
+class Artist(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Track(object):
+    def __init__(self, name, artist):
+        self.name = name
+        self.artist = artist
+
+
 class DoublerController(BaseController):
 
     def index(self, artist='The Temper Trap', track='Fader'):
         params = {'method': 'track.getSimilar',
                   'limit': 10,
+                  'format': 'json',
                   'api_key': LASTFM_API_KEY,
                   'artist': artist,
                   'track': track}
-        c.data = urllib.urlopen(LASTFM_API_PREFIX + urllib.urlencode(params)).read()
+
         c.tracks = []
+        data = json.loads(urllib.urlopen(LASTFM_API_PREFIX + urllib.urlencode(params)).read())        
+        for d in data['similartracks']['track']:
+            artist = Artist(d['artist']['name'])
+            track = Track(d['name'], artist)
+            c.tracks.append(track)
+            
         return render('doubler/index.html')
