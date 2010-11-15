a2cbca1d6bbbb1ab40507450412118b40b37e45a
ke4mtg <ke4mtg@ke4mtg-ubuntu.(none)>
2010-11-14 19:27:04 -0500

    added a scorer class, and an example rule function

--- double_my_playlist/double_my_playlist/controllers/doubler.py
+++ double_my_playlist/double_my_playlist/controllers/doubler.py
@@ -8,6 +8,7 @@ from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
 from double_my_playlist.lib.base import BaseController, render
+from double_my_playlist.lib.scorer import Scorer
 
 log = logging.getLogger(__name__)
 
@@ -23,24 +24,33 @@ class Track(object):
     def __init__(self, name, artist):
         self.name = name
         self.artist = artist
+    
+    def __str__(self):
+        return self.artist.name + ' - ' + self.name
+
+    def __repr__(self):
+        return "Track('" + self.name + "', '" + self.artist.name + "')"
 
 
 class TrackSearchResult(object):
     def __init__(self, track, score=100.0):
         self.track = track
         self.score = math.ceil(score)
+    
+    def __repr__(self):
+        return "TrackSearchResult('" + self.track.name + "', '" + str(self.score) + "')"
 
 
 class DoublerController(BaseController):
 
     def index(self, artist='The Temper Trap', track='Fader'):
-        def find_similar(artist, track):
+        def find_similar(track):
             params = {'method': 'track.getSimilar',
                       'limit': 25,
                       'format': 'json',
                       'api_key': LASTFM_API_KEY,
-                      'artist': artist,
-                      'track': track}
+                      'artist': track.artist.name,
+                      'track': track.name}
             data = json.loads(urllib.urlopen(LASTFM_API_PREFIX + urllib.urlencode(params)).read())
     
             results = []
@@ -56,8 +66,26 @@ class DoublerController(BaseController):
 
         known_tracks = [x.split(' - ') for x in c.playlist.split('\n')]
         c.results = []
+        originals = []
+        fetched = []
+
         for artist, track in known_tracks:
-            for similar_track in find_similar(artist, track):
-                c.results.append(similar_track)
-            
+            cur_track = Track(track,Artist(artist))
+            originals.append(cur_track)
+            for similar_track in find_similar(cur_track):
+        #        c.results.append(similar_track)
+                fetched.append(similar_track)
+        
+        scorer = Scorer(originals, fetched)
+        
+        def rule_everything_gets_five(originals, fetched):
+            results = []
+            for f in fetched:
+               results.append( TrackSearchResult( f.track, 5 ) )
+
+            return results
+       
+        scorer.add_rule(rule_everything_gets_five, 10)
+        c.results = scorer.score()[0]
+        
         return render('doubler/index.html')
